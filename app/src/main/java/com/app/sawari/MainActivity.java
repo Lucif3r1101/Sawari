22b6cd5eafaca0c52883e3843506a263e4e3205d
Lucif3r1101 <rishav1101@gmail.com>
2022-02-05 20:29:19 +0530

    First push

---
+++ app/src/main/java/com/app/sawari/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app.sawari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
